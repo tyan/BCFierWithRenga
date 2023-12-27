807009e60ae0d6f94aa86fa3cf22e30436bd4f8d
Tyan <tyan.evgeniy@gmail.com>
2023-12-27 17:50:58 +0300

    Add action to Renga primary panel

--- Bcfier.Renga/Entry/AppMain.cs
+++ Bcfier.Renga/Entry/AppMain.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Reflection;
-using System.Windows;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 using Renga;
 
@@ -12,80 +8,34 @@ namespace Bcfier.RengaPlugin.Entry
 {
   public class AppMain : IPlugin
   {
-    private string _path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
-    #region Renga IPlugin Implementation
-    public bool Initialize(string pluginFolder)
+    public bool Initialize(string plugInFolder)
     {
+      var app = new Renga.Application();
+      var ui = app.UI;
+
+      var actionImage = ui.CreateImage();
+      actionImage.LoadFromFile(Path.Combine(plugInFolder, "Assets/BCFierIcon16x16.png"));
+
+      var action = ui.CreateAction();
+      action.Icon = actionImage;
+      action.ToolTip = "BCFier";
+
+      var actionEvents = new Renga.ActionEventSource(action);
+      actionEvents.Triggered += (s, e) =>
+      {
+        var mainCommand = new CmdMain();
+        mainCommand.Execute();
+      };
+
+      var panelExtension = ui.CreateUIPanelExtension();
+      panelExtension.AddToolButton(action);
+
+      ui.AddExtensionToPrimaryPanel(panelExtension);
+
       return true;
     }
 
     public void Stop()
     {}
-
-    //public Result OnStartup(UIControlledApplication application)
-    //{
-    //  try
-    //  {
-    //    // Tab
-    //    RibbonPanel panel = application.CreateRibbonPanel("BCFier");
-
-    //    // Button Data
-    //    PushButton pushButton = panel.AddItem(new PushButtonData("BCFier",
-    //                                                                 "BCFier " + Assembly.GetExecutingAssembly().GetName().Version,
-    //                                                                 Path.Combine(_path, "Bcfier.Revit.dll"),
-    //                                                                 "Bcfier.Revit.Entry.CmdMain")) as PushButton;
-
-    //    // Images and Tooltip
-    //    if (pushButton != null)
-    //    {
-    //      pushButton.Image = LoadPngImgSource("Bcfier.Assets.BCFierIcon16x16.png", Path.Combine(_path, "Bcfier.dll"));
-    //      pushButton.LargeImage = LoadPngImgSource("Bcfier.Assets.BCFierIcon32x32.png", Path.Combine(_path, "Bcfier.dll"));
-    //      pushButton.ToolTip = "BCFier";
-    //    }
-    //  }
-    //  catch (Exception ex1)
-    //  {
-    //    MessageBox.Show("exception: " + ex1);
-    //    return Result.Failed;
-    //  }
-
-    //  return Result.Succeeded;
-    //}
-
-    //public Result OnShutdown(UIControlledApplication application)
-    //{
-    //  return Result.Succeeded;
-    //}
-
-    #endregion
-
-    #region Private Members
-
-    private ImageSource LoadPngImgSource(string sourceName, string path)
-    {
-
-      try
-      {
-        // Assembly & Stream
-        var assembly = Assembly.LoadFrom(Path.Combine(path));
-        var icon = assembly.GetManifestResourceStream(sourceName);
-
-        // Decoder
-        PngBitmapDecoder m_decoder = new PngBitmapDecoder(icon, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
-
-        // Source
-        ImageSource m_source = m_decoder.Frames[0];
-        return (m_source);
-      }
-      catch { }
-
-      // Fail
-      return null;
-
-    }
-
-    #endregion
-
   }
 }
